e69132b24b4ccf347f7e7f2af34c59b32b7a3049
PraneshxxTechy <120735988+PraneshxxTechy@users.noreply.github.com>
2023-11-23 22:31:28 +0530

    twosum.py

--- twosum.py
+++ twosum.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def twoSum(self, nums, target):
-       n=len(nums)
-       for i in range(n):
-            for j in range(i+1,n):
-                if(nums[i]+nums[j]==target):
+       n=len(nums) #Storing the length of the string for traversal.
+       for i in range(n): #This for loop iterates through the list.
+            for j in range(i+1,n): #Taken a number in the list using i index,this loop gives all the adjacent elements(forms a pair of elements)
+                if(nums[i]+nums[j]==target):#Checks for the target.
                  return [i,j]
